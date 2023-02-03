e9a6efcafeed5b6ee0e62024bcb6c73f1136e554
haojun <haojun.xu@goodwe.com>
2023-02-03 10:03:49 +0800

    init

--- blog.config.js
+++ blog.config.js
@@ -1,63 +1,71 @@
 const BLOG = {
-  title: 'Stimw Blog',
-  author: 'Stimw',
-  email: 'stimwblog@gmail.com',
-  link: 'https://notion-next-blog-olive.vercel.app', // do not end with '/'
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'light', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif'] - !!deprecated, do not modify this
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 7,
-  sortByDate: true,
-  showSummary: false,
-  showAbout: true,
-  showArchive: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/Pokemon',
-  seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '', // Remove the value or replace it with your own google site verification code
-  },
-  notionPageId:
-    process.env.NOTION_PAGE_ID || 'd3a1074627994f1b8475de6a919cb09c', // DO NOT CHANGE THIS！！！
-  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
-    ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '', // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
-    },
-    gaConfig: {
-      measurementId: '', // e.g: G-XXXXXXXXXX
-    },
-  },
-  comment: {
-    // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
-    gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
-      admin: [],
-      clientID: '',
-      clientSecret: '',
-      distractionFreeMode: false,
-    },
-    utterancesConfig: {
-      repo: '',
-    },
-    cusdisConfig: {
-      appId: '', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
-      scriptSrc: 'https://cusdis.com/js/cusdis.es.js', // change this if you're using self-hosted version
-    },
-  },
-  isProd: process.env.VERCEL_ENV === 'production', // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+	title: "RIG",
+	author: "RIG",
+	email: "owrig99@gmail.com",
+	link: "https://github.com/OWRIG", // do not end with '/'
+	description: " full stack developer",
+	lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+	appearance: "auto", // ['light', 'dark', 'auto'],
+	font: "sans-serif", // ['sans-serif', 'serif'] - !!deprecated, do not modify this
+	lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
+	darkBackground: "#18181B", // use hex value, don't forget '#'
+	path: "", // leave this empty unless you want to deploy Nobelium in a folder
+	since: 2022, // If leave this empty, current year will be used.
+	postsPerPage: 8,
+	sortByDate: true,
+	showSummary: false,
+	showAbout: true,
+	showArchive: true,
+	autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+	ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
+	socialLink: "https://twitter.com/Pokemon",
+	seo: {
+		keywords: [
+			"Blog",
+			"Website",
+			"Notion",
+			"RIG",
+			"OWRIG",
+			"每日奇思",
+			"OWRIG BLOG",
+		],
+		googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
+	},
+	notionPageId:
+		process.env.NOTION_PAGE_ID || "d3a1074627994f1b8475de6a919cb09c", // DO NOT CHANGE THIS！！！
+	notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
+	analytics: {
+		provider: "", // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+		ackeeConfig: {
+			tracker: "", // e.g 'https://ackee.craigary.net/tracker.js'
+			dataAckeeServer: "", // e.g https://ackee.craigary.net , don't end with a slash
+			domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+		},
+		gaConfig: {
+			measurementId: "", // e.g: G-XXXXXXXXXX
+		},
+	},
+	comment: {
+		// support provider: gitalk, utterances, cusdis
+		provider: "gitalk", // leave it empty if you don't need any comment plugin
+		gitalkConfig: {
+			repo: "nobeliumRIG", // The repository of store comments
+			owner: "OWRIG",
+			admin: ["OWRIG"],
+			clientID: "9f47acd49a0d9261adfe",
+			clientSecret: "ee7c01a434e0bd90b2f3adefdbbe9a511a3a402b",
+			distractionFreeMode: false,
+		},
+		utterancesConfig: {
+			repo: "",
+		},
+		cusdisConfig: {
+			appId: "", // data-app-id
+			host: "https://cusdis.com", // data-host, change this if you're using self-hosted version
+			scriptSrc: "https://cusdis.com/js/cusdis.es.js", // change this if you're using self-hosted version
+		},
+	},
+	isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 };
 // export default BLOG
 module.exports = BLOG;
